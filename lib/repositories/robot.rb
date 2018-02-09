7bc308a917b3bdaa14334daab70e5128627a4e3c
David Looi <david@cultureamp.com>
2018-02-10 10:55:45 +1100

    Add repositories

---
+++ lib/repositories/robot.rb
@@ -0,0 +1,10 @@
+module Repositories
+  class Robot < Value.new(:name, :condition, :configuration)
+  end
+
+  class Condition < Value.new(:on_fire, :rusty, :loose_screws, :paint_scratched)
+  end
+
+  class Configuration < Value.new(:has_sentience, :has_wheels, :has_tracks, :num_of_rotors, :colour)
+  end
+end
