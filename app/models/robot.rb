060d5df312e95f885e5c642e6556b447ea39bd67
David Looi <david@cultureamp.com>
2018-02-10 22:18:39 +1100

    Model to provide an interface to create a single robot

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,3 +1,20 @@
 class Robot < ApplicationRecord
   has_one :configuration
+
+  def self.create_with(name:, configuration:, condition:)
+    self.create(
+      name: name,
+      on_fire: condition.on_fire,
+      rusty: condition.rusty,
+      loose_screws: condition.loose_screws,
+      paint_scratched: condition.paint_scratched,
+      configuration: Configuration.new(
+        has_sentience: configuration.has_sentience,
+        has_wheels: configuration.has_wheels,
+        has_tracks: configuration.has_tracks,
+        num_of_rotors: configuration.num_of_rotors,
+        colour: configuration.colour,
+      )
+    )
+  end
 end
--- spec/models/robot_spec.rb
+++ spec/models/robot_spec.rb
@@ -1,5 +1,56 @@
 require 'rails_helper'
+require 'rspec/its'
 
 RSpec.describe Robot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.create_with' do
+    subject(:robot) do
+      described_class.create_with(
+        name: name,
+        configuration: configuration,
+        condition: condition,
+      )
+    end
+
+    let(:name) { 'BB-8' }
+    let(:configuration) do
+      Factory::FixedConfiguration.new(
+        has_sentience: true,
+        has_wheels: true,
+        has_tracks: true,
+        num_of_rotors: 5,
+        colour: 'purple',
+      )
+    end
+    let(:condition) do
+      Factory::FixedCondition.new(
+        on_fire: true,
+        rusty: true,
+        loose_screws: true,
+        paint_scratched: true,
+      )
+    end
+
+    it 'returns a Robot object' do
+      expect(robot.is_a?(Robot)).to be_truthy
+    end
+
+    context 'attributes' do
+      its(:id) { is_expected.to_not be_nil }
+      its(:name) { is_expected.to eq 'BB-8' }
+      its(:on_fire) { is_expected.to be_truthy }
+      its(:rusty) { is_expected.to be_truthy }
+      its(:loose_screws) { is_expected.to be_truthy }
+      its(:paint_scratched) { is_expected.to be_truthy }
+    end
+
+    context 'configuration' do
+      subject { robot.configuration }
+
+      its(:has_sentience) { is_expected.to be_truthy }
+      its(:has_wheels) { is_expected.to be_truthy }
+      its(:has_tracks) { is_expected.to be_truthy }
+      its(:num_of_rotors) { is_expected.to eq 5 }
+      its(:colour) { is_expected.to eq 'purple' }
+    end
+  end
 end
