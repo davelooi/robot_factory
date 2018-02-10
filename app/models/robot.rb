7db877ab461453232ba75010010fd5c182fefa30
David Looi <david@cultureamp.com>
2018-02-10 23:20:18 +1100

    Refactor Robot model to provide build_with and create_with method

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,20 +1,32 @@
 class Robot < ApplicationRecord
   has_one :configuration
 
-  def self.create_with(name:, configuration:, condition:)
-    self.create(
-      name: name,
-      on_fire: condition.on_fire,
-      rusty: condition.rusty,
-      loose_screws: condition.loose_screws,
-      paint_scratched: condition.paint_scratched,
-      configuration: Configuration.new(
-        has_sentience: configuration.has_sentience,
-        has_wheels: configuration.has_wheels,
-        has_tracks: configuration.has_tracks,
-        num_of_rotors: configuration.num_of_rotors,
-        colour: configuration.colour,
-      )
-    )
+  class << self
+    def create_with(name:, configuration:, condition:)
+      create(serialize(name: name, configuration: configuration, condition: condition))
+    end
+
+    def build_with(name:, configuration:, condition:)
+      new(serialize(name: name, configuration: configuration, condition: condition))
+    end
+
+    private
+
+    def serialize(name:, configuration:, condition:)
+      {
+        name: name,
+        on_fire: condition.on_fire,
+        rusty: condition.rusty,
+        loose_screws: condition.loose_screws,
+        paint_scratched: condition.paint_scratched,
+        configuration: Configuration.new(
+          has_sentience: configuration.has_sentience,
+          has_wheels: configuration.has_wheels,
+          has_tracks: configuration.has_tracks,
+          num_of_rotors: configuration.num_of_rotors,
+          colour: configuration.colour,
+        )
+      }
+    end
   end
 end
--- spec/models/robot_spec.rb
+++ spec/models/robot_spec.rb
@@ -2,40 +2,31 @@ require 'rails_helper'
 require 'rspec/its'
 
 RSpec.describe Robot, type: :model do
-  describe '.create_with' do
-    subject(:robot) do
-      described_class.create_with(
-        name: name,
-        configuration: configuration,
-        condition: condition,
-      )
-    end
+  let(:name) { 'BB-8' }
+  let(:configuration) do
+    Factory::FixedConfiguration.new(
+      has_sentience: true,
+      has_wheels: true,
+      has_tracks: true,
+      num_of_rotors: 5,
+      colour: 'purple',
+    )
+  end
+  let(:condition) do
+    Factory::FixedCondition.new(
+      on_fire: true,
+      rusty: true,
+      loose_screws: true,
+      paint_scratched: true,
+    )
+  end
 
-    let(:name) { 'BB-8' }
-    let(:configuration) do
-      Factory::FixedConfiguration.new(
-        has_sentience: true,
-        has_wheels: true,
-        has_tracks: true,
-        num_of_rotors: 5,
-        colour: 'purple',
-      )
-    end
-    let(:condition) do
-      Factory::FixedCondition.new(
-        on_fire: true,
-        rusty: true,
-        loose_screws: true,
-        paint_scratched: true,
-      )
-    end
-
-    it 'returns a Robot object' do
+  shared_examples 'a robot' do
+    it 'is a Robot' do
       expect(robot.is_a?(Robot)).to be_truthy
     end
 
     context 'attributes' do
-      its(:id) { is_expected.to_not be_nil }
       its(:name) { is_expected.to eq 'BB-8' }
       its(:on_fire) { is_expected.to be_truthy }
       its(:rusty) { is_expected.to be_truthy }
@@ -51,6 +42,38 @@ RSpec.describe Robot, type: :model do
       its(:has_tracks) { is_expected.to be_truthy }
       its(:num_of_rotors) { is_expected.to eq 5 }
       its(:colour) { is_expected.to eq 'purple' }
+    end
+  end
+
+  describe '.build_with' do
+    subject(:robot) do
+      described_class.build_with(
+        name: name,
+        configuration: configuration,
+        condition: condition,
+      )
+    end
+
+    it_behaves_like 'a robot'
+
+    it 'is not persisted' do
+      expect(robot.persisted?).to be_falsey
+    end
+  end
+
+  describe '.create_with' do
+    subject(:robot) do
+      described_class.create_with(
+        name: name,
+        configuration: configuration,
+        condition: condition,
+      )
+    end
+
+    it_behaves_like 'a robot'
+
+    it 'is persisted' do
+      expect(robot.persisted?).to be_truthy
     end
   end
 end
