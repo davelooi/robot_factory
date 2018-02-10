051e805235d32efe868be9f3559c6ed13611e5bf
David Looi <david@cultureamp.com>
2018-02-10 23:21:27 +1100

    Refactor to provide cleaner public interface

--- app/services/robot_factory.rb
+++ app/services/robot_factory.rb
@@ -2,19 +2,28 @@ module Services
   class RobotFactory
     class << self
       # returns an array of Robot
-      def build_batch(num_of_robots: 10)
+      def build_batch
         Robot.transaction do
-          num_of_robots.times.map do
-            Robot.create_with(
-              name: name,
-              configuration: configuration,
-              condition: condition,
-            )
-          end
+          build_robots(random_batch_quantity)
         end
       end
 
       private
+
+      # a batch can be anywhere from 10 to 1000 robots
+      def random_batch_quantity
+        Factory::Randomiser.number(10..1000)
+      end
+
+      def build_robots(batch_quantity)
+        batch_quantity.times.map do
+          Robot.create_with(
+            name: name,
+            configuration: configuration,
+            condition: condition,
+          )
+        end
+      end
 
       def name
         Faker::Superhero.name
--- spec/services/robot_factory_spec.rb
+++ spec/services/robot_factory_spec.rb
@@ -2,15 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Services::RobotFactory do
   describe '.build_batch' do
-    let(:robot) { Robot.new }
-    let(:num_of_robots) { 11 }
-    before { allow(Robot).to receive(:create_with).and_return(robot) }
+    subject(:batch) { described_class.build_batch }
 
-    subject(:batch) { described_class.build_batch(num_of_robots: num_of_robots) }
+    let(:batch_quantity) { 2 }
 
     it 'returns an array of robot' do
-      expect(batch.count).to eq num_of_robots
-      expect(batch).to include robot
+      expect(Factory::Randomiser).to receive(:number).with(10..1000).and_return(batch_quantity)
+      expect(Robot).to receive(:create_with).and_return(Robot.new).exactly(batch_quantity).times
+      expect(batch.count).to eq batch_quantity
     end
   end
 end
